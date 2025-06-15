3ca1e21fb03ce5a0173f263590a7f254a9c14ec9
Saranga Rasingolla <162134044+saranga423@users.noreply.github.com>
2025-06-15 13:32:22 +0530

    Add files via upload

--- client/src /components/Navbar.jsx
+++ client/src /components/Navbar.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Saranga Rasingolla</h1>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/projects">Projects</Link></li>
+      <li><Link to="/skills">Skills</Link></li>
+      <li><Link to="/resume">Resume</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
